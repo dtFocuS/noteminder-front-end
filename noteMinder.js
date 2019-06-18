bdc677402b95387215d86db6b6cbf59f2659f765
Danny Tseng <github email address>
2019-06-17 22:24:43 -0700

    working on notes section

--- noteMinder.js
+++ noteMinder.js
@@ -22,3 +22,16 @@ function addANewFolder() {
     li.setAttribute("href", "#")
     ul.appendChild(li)
 }
+
+function addNote() {
+  const addButton = document.querySelector("#add-note");
+  const notesSection = document.getElementById("note-detail");
+  addButton.addEventListener('click', (event) => {
+    showNote(notesSection);
+  })
+}
+
+function showNote(notesSection) {
+  const note = document.createElement("div");
+  
+}
