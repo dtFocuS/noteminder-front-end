f30ebe8dbd04e71637487fdfc615a0ff89337274
Danny Tseng <github email address>
2019-06-18 11:32:48 -0700

    displaying folders

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const FOLDERS_URL = "http://localhost:3000/api/v1/folders"
+const NOTES_URL = "http://localhost:3000/api/v1/notes"
+
 document.addEventListener("DOMContentLoaded", () => {
     main()
 
@@ -5,24 +8,50 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function main() {
     createNewFolder()
+    loadFolders();
     addNote();
+}
+
+function loadFolders() {
+  fetch(FOLDERS_URL)
+  .then(resp => resp.json())
+  .then(json => displayFolders(json))
+}
+
+function displayFolders(folders) {
+  folders.forEach(folder => displayFolder(folder))
+}
+
+function displayFolder(folder) {
+  const folderList = document.querySelector(".note-folders");
+  const listItem = document.createElement("li");
+  listItem.id = "folder-" + folder.id;
+  listItem.textContent = folder.name;
+  folderList.appendChild(listItem);
 }
 
 function createNewFolder() {
     let newFolder = document.querySelector(".btn")
+    const name = document.createElement("input");
+    let ul = document.querySelector(".note-folders")
+    let li = document.createElement('li')
+    name.type = "text";
 
     newFolder.addEventListener('click', (ev)  => {
-        ev.preventDefault()
-        addANewFolder()
+        ul.appendChild(name)
+        name.focus();
+        name.value = "New Folder"
+        addANewFolder(name)
     })
 }
 
-function addANewFolder() {
-    let ul = document.querySelector(".note-folders")
-    let li = document.createElement('li')
-    li.textContent = "Folder 2"
-    li.setAttribute("href", "#")
-    ul.appendChild(li)
+function addANewFolder(name) {
+
+
+
+    //li.textContent = "Folder 2"
+    //li.setAttribute("href", "#")
+
 }
 
 function addNote() {
