e876fa8fd31ee4cc41e1dd0f25d1c5e884564ce1
github username <github email address>
2019-06-20 19:34:49 -0700

    closes modal once button is pressed

--- index.js
+++ index.js
@@ -454,6 +454,7 @@ function showModal() {
   form.addEventListener('submit', (ev) => {
     ev.preventDefault();
     Myinterval = setInterval(setReminder, 1000, ev)
+    modal.style.display = "none";
   })
 }
 
