e2b561193619b3c1db0d4c039d7c5176e37203bc
Danny Tseng <dtfocus1025@gmail.com>
2020-01-03 19:15:25 -0800

    removed minimize and localindentname in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -29,9 +29,9 @@ module.exports = {
                         options: {
                             modules: true,
                             importLoaders: 1,
-                            localIdentName: "[name]_[local]_[hash:base64]",
+                            // localIdentName: "[name]_[local]_[hash:base64]",
                             sourceMap: true,
-                            minimize: true
+                            // minimize: true
                         }
                     }
                 ]
